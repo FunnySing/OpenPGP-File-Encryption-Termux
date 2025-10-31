d4aacb7adc5bc0948fff8646525ed965b9150bfc
FunnySing <officialluck@protonmail.com>
2025-10-31 17:41:10 +0000

    Update file_encryption.sh

--- file_encryption.sh
+++ file_encryption.sh
@@ -15,7 +15,7 @@ if [ $choice -eq 1 ]; then
     gpg -z 0 --cipher-algo AES256 --digest-algo SHA512 --s2k-mode 3 --s2k-count 65011712 --s2k-cipher-algo AES256 --s2k-digest-algo SHA512 --no-symkey-cache -c ""$input_file""
 
 elif [ $choice -eq 2 ]; then
-    read -p "Enter the Encrypted file name to decrypt (e.g.: file.zip.gpg): " encrypted_file
+    read -p "Enter the Encrypted file name (e.g.: file.zip.gpg): " encrypted_file
     read -p "Enter the Output file name (e.g.: file.zip): " decrypted_file
     echo
     gpg --no-symkey-cache -o ""$decrypted_file"" -d ""$encrypted_file""
